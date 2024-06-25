8eb739650d242bd236463670dd470159dcabff33
danish-111 <i220955@nu.edu.pk>
2024-06-25 21:39:03 +0500

    Added source files

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// My RAM Slot short circuited, had to reset my BIOS please understand
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout << "Entry point for the code\n";
+  return 0;
+}
---
+++ menu.h
@@ -0,0 +1,1 @@
+// I dont have my laptop so this is it for today sorry guys i know its cheap but anything for the streak
